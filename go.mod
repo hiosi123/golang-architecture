7bc987f823699a6db51a4c577e743b9136cf2089
hiosi123 <tls1641@gmail.com>
2023-04-10 22:04:53 +0900

    interface example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hiosi123/golang-architecture
+
+go 1.19
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func (p person) speak() {
+	fmt.Println("from a person - this is my name", p.first)
+}
+
+// any type that has the methods specified byan interface is also of the interface type
+// an interface says
+
+type human interface {
+	speak()
+}
+
 func main() {
-	fmt.Println("this is the git version 1.0.2")
+	p1 := person{
+		first: "James",
+	}
+
+	fmt.Printf("%T\n", p1)
+
+	//in go a Value can be of more than one TYPE
+	// in this example human p1 is both human and person
+	var x human
+	x = p1
+	fmt.Printf("%T\n", x)
 }
