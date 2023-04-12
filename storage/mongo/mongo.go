63b3d4121c6b431f6d28313cddf2b9aaa07c0328
hiosi123 <tls1641@gmail.com>
2023-04-12 23:25:40 +0900

    Organized Code

---
+++ storage/mongo/mongo.go
@@ -0,0 +1,13 @@
+package mongo
+
+import architecture "github.com/hiosi123/golang-architecture"
+
+type Db map[int]architecture.Person
+
+func (m Db) Save(n int, p architecture.Person) {
+	m[n] = p
+}
+
+func (m Db) Retrieve(n int) architecture.Person {
+	return m[n]
+}
